19920d8fe74c894f1808180611bbe4ab5ea16ca8
Amdrii <andreyk557@gmail.com>
2023-03-25 22:11:40 +0200

    feat[func-handlesub]

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,15 +1,35 @@
+import { FormEvent, useRef, useState } from "react";
 import { Form, FormGroup, Stack, Col, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
+import { NoteData, Tag } from "./App";
 
-export function NoteForm() {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+export function NoteForm({ onSubmit }: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
             <FormGroup controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required />
+              <Form.Control ref={titleRef} required />
             </FormGroup>
           </Col>
           <Col>
@@ -21,15 +41,17 @@ export function NoteForm() {
         </Row>
         <FormGroup controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control required as="textarea" rows={15} />
+          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
         </FormGroup>
-        <Stack direction="horizontal">
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
             Save
           </Button>
-          <Button type="button" variant="outline-secondary">
-            Cancel
-          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
         </Stack>
       </Stack>
     </Form>
