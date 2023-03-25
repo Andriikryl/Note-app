4a85257ddd56999b7a5d45b41359f769816d1c12
Amdrii <andreyk557@gmail.com>
2023-03-25 20:04:35 +0200

    git[init]

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
